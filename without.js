b5afcbc0214da44085860661266777969c1b5c32
Bowen Jiang <bopepsi@jiangguangweideMacBook-Pro.local>
2022-05-04 04:31:02 +0800

    without.js-added

---
+++ without.js
@@ -0,0 +1,17 @@
+const without = (words, filter) => {
+    let length = words.length;
+    for (var i = 0; i < length; i++) {
+        if (filter.includes(words[i])) {
+            words.splice(i, 1);
+            i--;
+            length = words.length;
+        }
+    }
+}
+
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouscleare"]); 
+console.log(words);
+// no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+// assertArraysEqual(words, ["hello", "world", "lighthouse"]);
