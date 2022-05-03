997b1544d9c2fcb50a7d83b52a79d808c17f972b
Bowen Jiang <bopepsi@jiangguangweideMacBook-Pro.local>
2022-05-04 03:57:25 +0800

    assertArraysEqual.js-added

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,9 +1,3 @@
-const assertEqual = function (actual, expected) {
-    if (actual === expected) console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-    else console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
-    return actual === expected;
-};
-
 const eqArrays = function (actual, expected) {
     if (actual.length) {
         for (var i = 0; i < actual.length; i++) {
@@ -18,9 +12,6 @@ const eqArrays = function (actual, expected) {
     return actual === expected;
 };
 
-// TEST CODE
-console.log(eqArrays("Lighthouse Labs", "Bootcamp"));
+console.log(eqArrays(['Lighthouse', 'Labs'], ['Bootcamp']));
 
-console.log(eqArrays(1, 1));
-
-console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, '3']), true));
+console.log(eqArrays([1,2,2],[1,2,3]));
