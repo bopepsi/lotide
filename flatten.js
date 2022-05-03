d3a767a27a31804d73e0336f90f0df7e1ea45623
Bowen Jiang <bopepsi@jiangguangweideMacBook-Pro.local>
2022-05-04 05:09:27 +0800

    flatten.js-added

---
+++ flatten.js
@@ -0,0 +1,10 @@
+const flatten = arr => {
+    let ans = [];
+    for (var item of arr) {
+        if (item.length) ans = ans.concat(flatten(item));
+        else ans.push(item);
+    }
+    return ans;
+}
+
+console.log(flatten([1, 2, [3, 4], 5, [6]])) // => [1, 2, 3, 4, 5, 6]
