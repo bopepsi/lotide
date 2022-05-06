5e9018efb544d76e00178a8ec995c11e0ee9c113
Bowen Jiang <bopepsi@jiangguangweideMacBook-Pro.local>
2022-05-07 02:53:44 +0800

    eqArrays.js-recursive-added

--- eqArrays.js
+++ eqArrays.js
@@ -5,18 +5,18 @@ const assertEqual = function (actual, expected) {
 };
 
 const eqArrays = function (actual, expected) {
-    if (actual.length) {
-        for (var i = 0; i < actual.length; i++) {
-            if (actual[i] !== expected[i]) {
-                console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
-                return false;
-            }
+    let ans = true;
+    const helper = (arr1, arr2) => {
+        if (arr1.length !== arr2.length) ans = false;
+        for (var i = 0; i < arr1.length; i++) {
+            if (Array.isArray(arr1[i])) helper(arr1[i], arr2[i]);
+            else { if (arr1[i] !== arr2[i]) ans = false; }
         };
-        console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-        return true;
-    };
-    return actual === expected;
+    }
+    helper(actual, expected);
+    return ans;
 };
+
 
 // TEST CODE
 console.log(eqArrays("Lighthouse Labs", "Bootcamp"));
@@ -24,3 +24,12 @@ console.log(eqArrays("Lighthouse Labs", "Bootcamp"));
 console.log(eqArrays(1, 1));
 
 console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, '3']), true));
+
+console.log('=====================================')
+
+
+console.log(eqArrays([2, [2, 3], [4]], [2, 2, [2, 3], [4]]))    // => true
+
+console.log(eqArrays([[2, 3], [4, 5]], [[2, 3], [4, 5]])) // => false
+
+console.log(eqArrays([[2, 3], [4]], [[2, 3], 4]))      // => false
