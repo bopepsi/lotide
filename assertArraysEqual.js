93d3ca6682c1169462c4d65b4469591d96e85f1f
Bowen Jiang <bopepsi@jiangguangweideMacBook-Pro.local>
2022-05-10 01:44:21 +0800

    assertArraysEqualTest.js-added

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,15 +1,23 @@
 const assertArraysEqual = function (actual, expected) {
-    if (actual.length) {
-        for (var i = 0; i < actual.length; i++) {
-            if (actual[i] !== expected[i]) {
-                console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
-                return false;
-            }
+
+    let ans = true;
+    const helper = (arr1, arr2) => {
+        if (arr1.length !== arr2.length) ans = false;
+        for (var i = 0; i < arr1.length; i++) {
+            if (Array.isArray(arr1[i])) helper(arr1[i], arr2[i]);
+            else { if (arr1[i] !== arr2[i]) ans = false; }
         };
+    }
+    helper(actual, expected);
+    if (ans) {
         console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
         return true;
-    };
-    return actual === expected;
+    }
+    if (!ans) {
+        console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
+        return false;
+    }
+
 };
 
 module.exports = assertArraysEqual;
